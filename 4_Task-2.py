3a493128710b41e9b773125af2da011f278626bc
gouthamgirish01-ui <gouthamgirish01@gmail.com>
2026-01-13 16:47:14 +0530

    Update 4_Task-2.py

--- 4_Task-2.py
+++ 4_Task-2.py
@@ -1,11 +1,15 @@
 try:
-    fh=open("output.txt","w")
-    a=input("Enter the text to write to the file: ")
-    fh.write(a)
-    fh.close()
-    fh=open("output.txt","a")
-    b=input("Enter additional text to append: ")
-    fh.append(b)
-    fh.close()
-except FileNotFoundError :
-    print("no such file exist")
+    with open("output.txt", "w") as fh:
+        a = input("Enter text to write to the file: ")
+        fh.write(a + "\n")
+        print("Data successfully written to output.txt.")
+    with open("output.txt", "a") as fh:
+        b = input("Enter additional text to append: ")
+        fh.write(b + "\n")
+        print("Data successfully appended.")
+    print("\nFinal content of output.txt:\n")
+    with open("output.txt", "r") as fh:
+        file_content = fh.read()
+        print(file_content)
+except FileNotFoundError:
+    print("No such file exists.")
